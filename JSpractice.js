d7ed26838e0ed28d60f6ae195a52c3a6b155d306
Allison <ayakosky@gmail.com>
2017-10-19 15:34:22 +0800

    testing repo

--- JSpractice.js
+++ JSpractice.js
@@ -2,4 +2,7 @@ console.log("hello");
 console.log("hello");
 console.log("hello");
 console.log("hello");
-console.log("hello");
+console.log("testing repo");
+console.log("testing repo");
+console.log("testing repo");
+console.log("testing repo");
