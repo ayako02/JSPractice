af3951d148a060793eaaa3e43134729a54a5bea9
ayako02 <4772706+ayako02@users.noreply.github.com>
2017-11-02 16:04:36 +0800

    updated with array, string, object

---
+++ ES6Learning/ES6Katas_Destructing.js
@@ -0,0 +1,113 @@
+// 10: destructuring - array
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('destructuring arrays makes shorter code', () => {
+
+  it('extract value from array, e.g. extract 0 into x like so `let [x] = [0];`', () => {
+    let [firstValue] = [1];
+    assert.strictEqual(firstValue, 1);
+  });
+
+  it('swap two variables, in one operation', () => {
+    let [x, y] = ['ax', 'why'];
+    [x, y] = [y, x];
+    assert.deepEqual([x, y], ['why', 'ax']);
+  });
+  
+  it('leading commas', () => {
+    const all = ['ax', 'why', 'zet'];
+    const [,,z] = all;
+    assert.equal(z, 'zet');
+  });
+  
+  it('extract from nested arrays', () => {
+    const user = [['Some', 'One'], 23];
+    const [[firstName, surname], age] = user;
+    
+    //another way to solve
+    //const user = [...['Some', 'One'], 23];    
+    // const [firstName, surname, age] = user;
+    
+    const expected = 'Some One = 23 years';
+    assert.equal(`${firstName} ${surname} = ${age} years`, expected);
+  });
+
+  it('chained assignments', () => {
+    let c, d;
+    let [a, b] = [c, d] = [1, 2];
+    assert.deepEqual([a, b, c, d], [1, 2, 1, 2]);
+  });
+
+  it('in for-of loop', () => {
+    for (var [, a, b] of [[0, 1, 2]]) {}
+    assert.deepEqual([a, b], [1, 2]);
+  });
+
+});
+
+
+// 11: destructuring - string
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('destructuring also works on strings', () => {
+
+  
+  it('destructure every character', () => {
+    let [a, b, c] = 'abc';
+    assert.deepEqual([a, b, c], ['a', 'b', 'c']);
+  });
+  
+  it('missing characters are undefined', () => {
+    const [a, c] = '';
+    assert.equal(c, void 0);
+  });
+  
+  it('unicode character work too', () => {
+    const [space, coffee] = 'a☕';
+    assert.equal(coffee, '\u{2615}');
+  });
+  
+});
+
+
+// 12: destructuring - object
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('destructuring objects', () => {
+
+  it('is simple', () => {
+    const {x} = {x: 1};
+    assert.equal(x, 1);
+  });
+
+  describe('nested', () => {
+    it('multiple objects', () => {
+      const magic = {first: 23, second: 42};
+      const {magic: {second}} = {magic};
+      assert.equal(second, 42);
+    });
+    it('object and array', () => {
+      const {z:[,x]} = {z: [23, 42]};
+      assert.equal(x, 42);
+    });
+    it('array and object', () => {
+      const [,[{lang}]] = [null, [{env: 'browser', lang: 'ES6'}]];
+      assert.equal(lang, 'ES6');
+    });
+  });
+  
+  describe('interesting', () => {
+    it('missing refs become undefined', () => {
+      //const {z} = {x: 1, z: 2};
+      const {z} = {x: 1};       
+      //const {z} = {x: 1, y: 2};
+      assert.equal(z, void 0);
+    });
+  
+    it('destructure from builtins (string)', () => {
+      const {substr} = '1';
+      assert.equal(substr, String.prototype.substr);
+    });
+  });
+
+});
